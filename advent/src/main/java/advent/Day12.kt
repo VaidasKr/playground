8d375c2251de44a832e44593f957198f1f6585d7
VaidasKr <vaidas.krisceliunas@telesoftas.com>
2022-12-12 15:43:26 +0200

    Update Day12.kt

--- advent/src/main/java/advent/Day12.kt
+++ advent/src/main/java/advent/Day12.kt
@@ -3,7 +3,6 @@ package advent
 object Day12 {
     fun shortestDistanceFromStoE(input: String): Int {
         var start = Position(0, 0)
-        var end = Position(0, 0)
         val map = input.replace(" ", "").split("\n")
             .mapIndexed { indexY, line ->
                 line.mapIndexed { indexX, char ->
@@ -12,30 +11,21 @@ object Day12 {
                             start = Position(indexX, indexY)
                             'a'
                         }
-                        'E' -> {
-                            end = Position(indexX, indexY)
-                            'z'
-                        }
                         else -> char
                     }
                     value
                 }
             }
-        return shortestPath(map, start, end)
+        return shortestPath(map, start)
     }
 
     fun shortestDistanceFromAnyAtoE(input: String): Int {
         val starts = mutableListOf<Position>()
-        var end = Position(0, 0)
         val map = input.replace(" ", "").split("\n")
             .mapIndexed { indexY, line ->
                 line.mapIndexed { indexX, char ->
                     val value = when (char) {
                         'S' -> 'a'
-                        'E' -> {
-                            end = Position(indexX, indexY)
-                            'z'
-                        }
                         else -> char
                     }
                     if (value == 'a') {
@@ -44,13 +34,12 @@ object Day12 {
                     value
                 }
             }
-        return starts.minOf { start -> shortestPath(map, start, end) }
+        return starts.minOf { start -> shortestPath(map, start) }
     }
 
     private fun shortestPath(
         map: List<List<Char>>,
-        start: Position,
-        end: Position
+        start: Position
     ): Int {
         val height = map.size
         val width = map.first().size
@@ -62,8 +51,8 @@ object Day12 {
             ongoingPositions.forEach { position ->
                 val charAt = map[position.y][position.x]
                 position.onEdge(width, height) { edge ->
-                    if (edge == end && charAt >= 'y') return steps + 1
                     val edgeValue = map[edge.y][edge.x]
+                    if (edgeValue == 'E' && charAt >= 'y') return steps + 1
                     if (edgeValue - charAt <= 1 && visitedPositions.add(edge)) newOngoing.add(edge)
                 }
             }
@@ -75,10 +64,10 @@ object Day12 {
 
     data class Position(val x: Int, val y: Int) {
         inline fun onEdge(width: Int, height: Int, onEdge: (Position) -> Unit) {
-            if (x > 0) onEdge(Position(x - 1, y))
-            if (x < width - 1) onEdge(Position(x + 1, y))
-            if (y > 0) onEdge(Position(x, y - 1))
-            if (y < height - 1) onEdge(Position(x, y + 1))
+            if (x > 0) onEdge(copy(x = x - 1))
+            if (x < width - 1) onEdge(copy(x = x + 1))
+            if (y > 0) onEdge(copy(y = y - 1))
+            if (y < height - 1) onEdge(copy(y = y + 1))
         }
     }
 }
