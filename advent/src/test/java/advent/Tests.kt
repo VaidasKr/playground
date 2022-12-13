9fa702483fb2e064fec48346de7f97d54b5a12a7
VaidasKr <vaidas.krisceliunas@telesoftas.com>
2022-12-13 04:33:48 +0200

    Move input from Day12Test.kt to day12Input and day12Sample files

--- advent/src/test/java/advent/Tests.kt
+++ advent/src/test/java/advent/Tests.kt
@@ -9,3 +9,13 @@ fun Int.assert(expected: Int) {
 fun Int.print() {
     println(this)
 }
+
+fun <T : Any> T.fileStream(name: String) = javaClass.classLoader.getResourceAsStream(name)!!.bufferedReader()
+
+fun <T : Any> T.readFile(name: String): String = fileStream(name).readText().trimEnd()
+
+fun <T : Any> T.readFileLines(name: String): List<String> = fileStream(name).readLines()
+
+fun <T : Any> T.fileLines(name: String, onLine: (String) -> Unit): Sequence<String> {
+    return fileStream(name).lineSequence()
+}
