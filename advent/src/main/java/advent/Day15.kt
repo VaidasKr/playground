bcc3edbc2b9c29966f07969da5e2b356d1f93555
VaidasKr <vaidas.krisceliunas@telesoftas.com>
2022-12-15 12:06:42 +0200

    Refactoring for Day15.kt

--- advent/src/main/java/advent/Day15.kt
+++ advent/src/main/java/advent/Day15.kt
@@ -2,107 +2,88 @@ package advent
 
 import kotlin.math.absoluteValue
 
-object Day15 {
-    fun noBeaconAtLine(input: String, lineNumber: Int): Int {
-        val knownBeaconAtLine = hashSetOf<Int>()
-        val noPossibleBeaconsOnLine = hashSetOf<Int>()
-        input.trim().split("\n").forEach { line ->
-            var xIndex = line.indexOf('x')
-            var yIndex = line.indexOf('y')
-            val separator = line.indexOf(':')
-            val sensorX = line.substring(xIndex + 2, yIndex - 2).toInt()
-            val sensorY = line.substring(yIndex + 2, separator).toInt()
-            xIndex = line.indexOf('x', separator)
-            yIndex = line.indexOf('y', separator)
-            val beaconX = line.substring(xIndex + 2, yIndex - 2).toInt()
-            val beaconY = line.substring(yIndex + 2, line.length).toInt()
-            if (beaconY == lineNumber) knownBeaconAtLine.add(beaconX)
-            val distance = (sensorX - beaconX).absoluteValue + (sensorY - beaconY).absoluteValue
-            val distanceToLine = (sensorY - lineNumber).absoluteValue
-            val dif = distance - distanceToLine
-            if (dif > 0) {
-                val from = sensorX - dif
-                val to = sensorX + dif
-                for (x in from..to) {
-                    noPossibleBeaconsOnLine.add(x)
-                }
-            }
-            println()
-        }
-        return noPossibleBeaconsOnLine.filter { !knownBeaconAtLine.contains(it) }.size
-    }
+class Day15(input: String) {
+    private val sensorCount: Int
+    private val sensors: IntArray
+    private val sensorDistance: IntArray
+    private val beacons: IntArray
 
-    fun possibleBeaconFreq2(input: String, size: Int): Long {
+    init {
         val split = input.trim().split("\n")
-        val sensorCount = split.size
-        val sensorX = IntArray(sensorCount)
-        val sensorY = IntArray(sensorCount)
-        val sensorDistance = IntArray(sensorCount)
+        sensorCount = split.size
+        sensors = IntArray(sensorCount * 2)
+        beacons = IntArray(sensorCount * 2)
+        sensorDistance = IntArray(sensorCount)
         split.forEachIndexed { index, line ->
             var xIndex = line.indexOf('x')
             var yIndex = line.indexOf('y')
             val separator = line.indexOf(':')
-            sensorX[index] = line.substring(xIndex + 2, yIndex - 2).toInt()
-            sensorY[index] = line.substring(yIndex + 2, separator).toInt()
+            sensors[2 * index] = line.substring(xIndex + 2, yIndex - 2).toInt()
+            sensors[2 * index + 1] = line.substring(yIndex + 2, separator).toInt()
             xIndex = line.indexOf('x', separator)
             yIndex = line.indexOf('y', separator)
-            val beaconX = line.substring(xIndex + 2, yIndex - 2).toInt()
-            val beaconY = line.substring(yIndex + 2, line.length).toInt()
-            sensorDistance[index] = (sensorX[index] - beaconX).absoluteValue + (sensorY[index] - beaconY).absoluteValue
+            beacons[2 * index] = line.substring(xIndex + 2, yIndex - 2).toInt()
+            beacons[2 * index + 1] = line.substring(yIndex + 2, line.length).toInt()
+            sensorDistance[index] =
+                (sensors[2 * index] - beacons[2 * index]).absoluteValue +
+                    (sensors[2 * index + 1] - beacons[2 * index + 1]).absoluteValue
         }
-        return frequency(size, sensorX, sensorY, sensorDistance)
     }
 
-    private fun frequency(size: Int, sensorX: IntArray, sensorY: IntArray, sensorDistance: IntArray): Long {
-        var y = 0
+    fun noPossibleBeaconCountOn(lineNumber: Int): Int {
+        val usedPoints = hashSetOf<Int>()
+        for (i in 0 until sensorCount) {
+            if (beacons[i * 2 + 1] == lineNumber) {
+                usedPoints.add(beacons[i * 2])
+            }
+        }
+        var count = 0
+        for (i in 0 until sensorCount) {
+            val sensorX = sensors[i * 2]
+            val sensorY = sensors[i * 2 + 1]
+            val distanceToLine = (sensorY - lineNumber).absoluteValue
+            val dif = sensorDistance[i] - distanceToLine
+            if (dif > 0) {
+                val from = sensorX - dif
+                val to = sensorX + dif
+                for (x in from..to) {
+                    if (usedPoints.add(x)) count++
+                }
+            }
+        }
+        return count
+    }
+
+    fun possibleBeaconFrequency(size: Int): Long {
         var x: Int
-        var nextX: Int
+        var lastX: Int
         var iterations = 0
-        while (y <= size) {
+        for (y in 0..size) {
             x = 0
             while (x <= size) {
                 iterations++
-                nextX = nextXPosForLine(sensorX, sensorY, sensorDistance, x, y)
-                if (nextX == -1) {
+                lastX = lastXforNearestSensor(x, y)
+                if (lastX == -1) {
                     println("iterations: $iterations x: $x y: $y")
                     return x * 4_000_000L + y
                 }
-                x = nextX
+                x = lastX + 1
             }
-            y++
         }
         return 0
     }
 
-    private fun nextXPosForLine(
-        sensorsX: IntArray,
-        sensorsY: IntArray,
-        sensorsDistance: IntArray,
-        x: Int,
-        y: Int
-    ): Int {
-        for (i in sensorsX.indices) {
-            val distance = (sensorsX[i] - x).absoluteValue + (sensorsY[i] - y).absoluteValue
-            if (distance <= sensorsDistance[i]) {
-                val distanceToLine = (sensorsY[i] - y).absoluteValue
-                val dif = sensorsDistance[i] - distanceToLine
-                return sensorsX[i] + dif + 1
+    private fun lastXforNearestSensor(x: Int, y: Int): Int {
+        for (i in 0 until sensorCount) {
+            val sensorX = sensors[i * 2]
+            val sensorY = sensors[i * 2 + 1]
+            val distance = (sensorX - x).absoluteValue + (sensorY - y).absoluteValue
+            if (distance <= sensorDistance[i]) {
+                val distanceToLine = (sensorY - y).absoluteValue
+                val dif = sensorDistance[i] - distanceToLine
+                return sensorX + dif
             }
         }
         return -1
-    }
-
-    private fun inSensorBounds(
-        sensorX: IntArray,
-        sensorY: IntArray,
-        sensorDistance: IntArray,
-        x: Int,
-        y: Int,
-    ): Boolean {
-        for (i in sensorX.indices) {
-            val distance = (sensorX[i] - x).absoluteValue + (sensorY[i] - y).absoluteValue
-            if (distance <= sensorDistance[i]) return true
-        }
-        return false
     }
 }
--- advent/src/test/java/advent/Day15Test.kt
+++ advent/src/test/java/advent/Day15Test.kt
@@ -4,22 +4,18 @@ import org.junit.Test
 
 class Day15Test {
     @Test
-    fun sample1() {
-        Day15.noBeaconAtLine(readFile("day15sample"), 10).assert(26)
+    fun sample() {
+        Day15(readFile("day15sample")).apply {
+            noPossibleBeaconCountOn(10).assert(26)
+            possibleBeaconFrequency(20).assert(56000011)
+        }
     }
 
     @Test
-    fun sample2() {
-        Day15.possibleBeaconFreq2(readFile("day15sample"), 20).assert(56000011)
-    }
-
-    @Test
-    fun actual1() {
-        Day15.noBeaconAtLine(readFile("day15"), 2000000).print()
-    }
-
-    @Test
-    fun actual2() {
-        Day15.possibleBeaconFreq2(readFile("day15"), 4_000_000).assert(11747175442119)
+    fun actual() {
+        Day15(readFile("day15")).apply {
+            noPossibleBeaconCountOn(2000000).assert(6275922)
+            possibleBeaconFrequency(4_000_000).assert(11747175442119)
+        }
     }
 }
