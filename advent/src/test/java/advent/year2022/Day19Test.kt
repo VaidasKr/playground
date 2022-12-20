b569ba0a707aa5a6abd9ec5341b7623697dac86b
VaidasKr <vaidas.krisceliunas@telesoftas.com>
2022-12-20 03:12:38 +0200

    Add test measurements

--- advent/src/test/java/advent/year2022/Day19Test.kt
+++ advent/src/test/java/advent/year2022/Day19Test.kt
@@ -3,9 +3,8 @@ package advent.year2022
 import advent.assert
 import advent.readFile
 import org.junit.Test
-import java.util.concurrent.Executors
-import java.util.concurrent.Future
 import java.util.concurrent.atomic.AtomicInteger
+import kotlin.system.measureNanoTime
 
 class Day19Test {
     @Test
@@ -16,40 +15,32 @@ class Day19Test {
         val day19 = Day19(sampleInput)
         day19.blueprints.first().searchDecisionBased(24).assert(9)
         day19.blueprints[1].searchDecisionBased(24).assert(12)
-//        day19.qualityLevelSum(24).assert(33)
     }
 
     @Test
     fun actual() {
         val day19 = Day19(readFile("2022/day19"))
         val sum = AtomicInteger()
-        val scheduler = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())
-        val futures = mutableListOf<Future<*>>()
         for (i in day19.blueprints.indices) {
-            futures.add(
-                scheduler.submit {
-                    val bluePrint = day19.blueprints[i]
-                    val actual = bluePrint.searchDecisionBased(24)
-                    sum.updateAndGet { it + bluePrint.id * actual }
-                }
-            )
+            val bluePrint = day19.blueprints[i]
+            val actual = bluePrint.searchDecisionBased(24)
+            sum.updateAndGet { it + bluePrint.id * actual }
         }
-        for (future in futures) {
-            future.get()
+        for (blueprint in day19.blueprints) {
+            val decision = measureNanoTime { blueprint.searchDecisionBased(24) } to "deci"
+            val fast = measureNanoTime { blueprint.searchFast(24) } to "fast"
+//            val step = measureNanoTime { blueprint.searchStepBased(24) } to "step"
+            println("blueprint ${blueprint.id}")
+            listOf(decision, fast).sortedBy { it.first }
+                .forEach { (time, name) -> println("$name - $time") }
+            println()
         }
         sum.get().assert(1346)
         sum.set(1)
         for (i in 0..2) {
-            futures.add(
-                scheduler.submit {
-                    val bluePrint = day19.blueprints[i]
-                    val actual = bluePrint.searchDecisionBased(32)
-                    sum.updateAndGet { it * actual }
-                }
-            )
-        }
-        for (future in futures) {
-            future.get()
+            val bluePrint = day19.blueprints[i]
+            val actual = bluePrint.searchDecisionBased(32)
+            sum.updateAndGet { it * actual }
         }
         sum.get().assert(7644)
     }
