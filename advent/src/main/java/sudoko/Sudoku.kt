a35db1d759c1d3b12444f7f925570c6434611956
VaidasKr <vaidas.krisceliunas@telesoftas.com>
2023-03-02 10:52:17 +0200

    Add sudoku stuff

---
+++ advent/src/main/java/sudoko/Sudoku.kt
@@ -0,0 +1,223 @@
+package sudoko
+
+@JvmInline
+value class Sudoku(val array: IntArray) {
+    fun iterate(): Boolean = hasUpdatesInRange(1..9) { number -> write(number) }
+
+    private inline fun hasUpdatesInRange(range: IntRange, updateCheck: (Int) -> Boolean): Boolean {
+        var updated = false
+        for (number in range) updated = updateCheck(number) || updated
+        return updated
+    }
+
+    private fun write(number: Int): Boolean = writeSquares(number) || writeRows(number) || writeColumns(number)
+
+    fun writeSquares(number: Int): Boolean = hasUpdatesInRange(0..2) { row ->
+        hasUpdatesInRange(0..2) { col -> writeSquare(row * 3, col * 3, number) }
+    }
+
+    private fun writeSquare(row: Int, col: Int, number: Int): Boolean {
+        if (isInSquare(row, col, number)) return false
+        var possibleIndex = -1
+        for (r in row..row + 2) {
+            for (c in col..col + 2) {
+                if (valueAt(r, c) != 0) {
+                    continue
+                }
+                if (!isInRow(r, number) && !isInColumn(c, number)) {
+                    if (possibleIndex == -1) {
+                        val indexOf = indexOf(r, c)
+                        possibleIndex = indexOf
+                    } else {
+                        return false
+                    }
+                }
+            }
+        }
+        return if (possibleIndex == -1) {
+            false
+        } else {
+            array[possibleIndex] = number
+            true
+        }
+    }
+
+    private fun writeRows(number: Int): Boolean = hasUpdatesInRange(0..8) { row -> writeRow(row, number) }
+
+    private fun writeRow(row: Int, number: Int): Boolean {
+        if (isInRow(row, number)) return false
+        var possibleCol = -1
+        for (col in 0..8) {
+            if (valueAt(row, col) != 0) continue
+            if (!isInSquare(row, col, number) && !isInColumn(col, number)) {
+                if (possibleCol == -1) {
+                    possibleCol = col
+                } else {
+                    return false
+                }
+            }
+        }
+        return if (possibleCol == -1) {
+            false
+        } else {
+            writeAt(row, possibleCol, number)
+            true
+        }
+    }
+
+
+    private fun writeColumns(number: Int): Boolean = hasUpdatesInRange(0..8) { column -> writeColumn(column, number) }
+
+    private fun writeColumn(column: Int, number: Int): Boolean {
+        if (isInColumn(column, number)) return false
+        var possibleRow = -1
+        for (row in 0..8) {
+            if (valueAt(row, column) != 0) continue
+            if (!isInSquare(row, column, number) && !isInRow(row, number)) {
+                if (possibleRow == -1) {
+                    possibleRow = row
+                } else {
+                    return false
+                }
+            }
+        }
+        return if (possibleRow == -1) {
+            false
+        } else {
+            writeAt(possibleRow, column, number)
+            true
+        }
+    }
+
+    fun print() {
+        var index = 0
+        while (index < 81) {
+            val value = array[index++]
+            if (value == 0) {
+                print('.')
+            } else {
+                print(value)
+            }
+            print(' ')
+
+            if (index % 27 == 0) {
+                println()
+                println("--------------------")
+            } else if (index % 9 == 0) {
+                println()
+            } else if (index % 3 == 0) {
+                print('|')
+            }
+        }
+        println()
+    }
+
+    override fun toString(): String = buildString {
+        append('[')
+        var index = 0
+        repeatWithSeparator(
+            times = 9,
+            action = {
+                append('[')
+                repeatWithSeparator(
+                    9,
+                    {
+                        append('"')
+                        val value = array[index++]
+                        if (value == 0) {
+                            append('.')
+                        } else {
+                            append(value)
+                        }
+                        append('"')
+                    },
+                    {
+                        append(',')
+                    }
+                )
+                append(']')
+            }, separator = {
+                append(',')
+            }
+        )
+        append(']')
+    }
+
+    fun isInRowAt(index: Int, value: Int): Boolean = isInRow(rowAtIndex(index), value)
+
+    private fun isInRow(row: Int, value: Int): Boolean {
+        for (col in 0..8) {
+            if (valueAt(row, col) == value) return true
+        }
+        return false
+    }
+
+    fun isInColumnAt(index: Int, value: Int): Boolean = isInColumn(columnAtIndex(index), value)
+
+    private fun isInColumn(column: Int, value: Int): Boolean {
+        for (row in 0..8) {
+            if (valueAt(row, column) == value) return true
+        }
+        return false
+    }
+
+    fun isInSquareAt(index: Int, value: Int): Boolean {
+        val squareCol = columnAtIndex(index) / 3 * 3
+        val squareRow = rowAtIndex(index) / 3 * 3
+
+        for (col in squareCol..squareCol + 2) {
+            for (row in squareRow..squareRow + 2) {
+                if (valueAt(row, col) == value) return true
+            }
+        }
+        return false
+    }
+
+    private fun isInSquare(row: Int, col: Int, value: Int): Boolean = isInSquareAt(indexOf(row, col), value)
+
+    private fun rowAtIndex(index: Int) = index / 9
+
+    private fun columnAtIndex(index: Int) = index % 9
+
+    private fun indexOf(row: Int, col: Int): Int = row * 9 + col
+
+    private fun valueAt(row: Int, col: Int): Int = array[indexOf(row, col)]
+
+    private fun writeAt(row: Int, col: Int, value: Int) {
+        array[indexOf(row, col)] = value
+    }
+
+    private inline fun repeatWithSeparator(times: Int, action: () -> Unit, separator: () -> Unit) {
+        for (i in 2..times) {
+            action()
+            separator()
+        }
+        action()
+    }
+
+    companion object {
+        fun parse(input: String): Sudoku {
+            val array = IntArray(81)
+            val table = Sudoku(array)
+            var index = 0
+            input.replace("\"", "").replace("[", "").replace("]", "").split(",").forEach { value ->
+                if (value == ".") {
+                    array[index++] = 0
+                } else {
+                    array[index++] = value.toInt()
+                }
+            }
+            return table
+        }
+
+        fun solve(input: String): String {
+            val table = parse(input)
+            table.print()
+
+            while (table.iterate()) {
+                table.print()
+            }
+            return table.toString()
+        }
+    }
+}
---
+++ advent/src/test/java/sudoko/SudokuTest.kt
@@ -0,0 +1,141 @@
+package sudoko
+
+import advent.assert
+import advent.assertFalse
+import advent.assertTrue
+import org.junit.Assert
+import org.junit.Test
+
+class SudokuTest {
+
+    private val input = """
+                [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+            """.trimIndent()
+
+    private val hardInput =
+        """[[".",".","2",".","9","4","5",".","."],[".",".",".","1",".",".","8","2","."],["3",".",".",".",".","7",".","4","."],["1",".",".","7",".",".",".",".","2"],[".",".",".",".","5","1",".",".","4"],["6",".",".",".","4",".",".","8","5"],[".",".","7",".",".","2",".","9","."],[".",".",".",".",".",".",".",".","."],[".",".","4",".",".",".",".",".","3"]]"""
+
+    @Test
+    fun actual() {
+        Sudoku.solve(input).assert(
+            """
+                [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
+            """.trimIndent()
+        )
+    }
+
+    @Test
+    fun `parse-format`() {
+        val actual = Sudoku.parse(input).toString()
+
+        Assert.assertEquals(input, actual)
+    }
+
+    @Test
+    fun checkInRow() {
+        val table = Sudoku.parse(input)
+        val numbersInRows = arrayOf(
+            intArrayOf(5, 3, 7),
+            intArrayOf(6, 1, 9, 5),
+            intArrayOf(9, 8, 6),
+
+            intArrayOf(8, 6, 3),
+            intArrayOf(4, 8, 3, 1),
+            intArrayOf(7, 2, 6),
+
+            intArrayOf(6, 2, 8),
+            intArrayOf(4, 1, 9, 5),
+            intArrayOf(8, 7, 9),
+        )
+        for (column in 0..8) {
+            val inRow = numbersInRows[column]
+            for (row in 0..8) {
+                val index = row + 9 * column
+                for (number in 1..9) {
+                    val result = inRow.contains(number)
+                    Assert.assertEquals(result, table.isInRowAt(index, number))
+                }
+            }
+        }
+    }
+
+    @Test
+    fun checkInColumn() {
+        val table = Sudoku.parse(input)
+        val numbersInColumn = arrayOf(
+            intArrayOf(5, 6, 8, 4, 7),
+            intArrayOf(3, 9, 6),
+            intArrayOf(8),
+            intArrayOf(1, 8, 4),
+            intArrayOf(7, 9, 6, 2, 1, 8),
+            intArrayOf(5, 3, 9),
+            intArrayOf(2),
+            intArrayOf(6, 8, 7),
+            intArrayOf(3, 1, 6, 5, 9)
+        )
+        for (row in 0..8) {
+            val inColumn = numbersInColumn[row]
+            for (column in 0..8) {
+                val index = row + 9 * column
+                for (number in 1..9) {
+                    val result = inColumn.contains(number)
+                    Assert.assertEquals(result, table.isInColumnAt(index, number))
+                }
+            }
+        }
+    }
+
+    @Test
+    fun checkInSquare() {
+        val table = Sudoku.parse(input)
+        val squareOffset = intArrayOf(0, 3, 6, 27, 30, 33, 54, 57, 60)
+        val squareIndexed = intArrayOf(0, 1, 2, 9, 10, 11, 18, 19, 20)
+        val numbersInSquares = arrayOf(
+            intArrayOf(5, 3, 6, 9, 8),
+            intArrayOf(1, 7, 9, 5),
+            intArrayOf(6),
+
+            intArrayOf(8, 4, 7),
+            intArrayOf(8, 6, 3, 2),
+            intArrayOf(3, 1, 6),
+
+            intArrayOf(6),
+            intArrayOf(4, 1, 9, 8),
+            intArrayOf(2, 8, 5, 7, 9)
+        )
+        numbersInSquares.forEachIndexed { squareIndex, inSquare ->
+            val offset = squareOffset[squareIndex]
+            squareIndexed.forEach { i ->
+                val index = i + offset
+                for (number in 1..9) {
+                    val result = inSquare.contains(number)
+                    Assert.assertEquals(result, table.isInSquareAt(index, number))
+                }
+            }
+        }
+    }
+
+    @Test
+    fun checkIteration() {
+        Sudoku.parse(hardInput).iterate().assertTrue()
+    }
+
+    @Test
+    fun `solve-hard`() {
+        Sudoku.solve(hardInput).contains('.').assertFalse()
+    }
+
+    @Test
+    fun `write-squares`() {
+        Sudoku.parse(hardInput).apply {
+            print()
+            for (i in 1..9) {
+                println("writing $i")
+                println()
+                if (writeSquares(i)) {
+                    print()
+                }
+            }
+        }
+    }
+}
