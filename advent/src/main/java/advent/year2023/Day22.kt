d7d263b76428b5a05117159a21fa51dd1e0fe284
VaidasKr <vaidas.krisceliunas@telesoftas.com>
2023-12-22 12:52:15 +0200

    Update methods for Day22

--- advent/src/main/java/advent/year2023/Day22.kt
+++ advent/src/main/java/advent/year2023/Day22.kt
@@ -2,31 +2,22 @@ package advent.year2023
 
 object Day22 {
     fun safeToDisintegrateBricks(input: String): Int {
-        val bricks = input.trim().split('\n').map { line ->
-            val (start, end) = line.split('~').map { it.split(',').map { it.toInt() } }
-            val points = IntArray(6)
-            for (i in 0 until 3) {
-                points[i] = start[i].coerceAtMost(end[i])
-                points[i + 3] = start[i].coerceAtLeast(end[i])
-            }
-            points
-        }.sortedBy { it[2] }
+        val bricks = parseBricks(input)
 
-        val supportedList = Array<List<Int>>(bricks.size) { emptyList() }
         val brickSet = bricks.indices.toHashSet()
         for (i in bricks.indices) {
             val brick = bricks[i]
             if (brick[2] == 1) {
                 continue
             }
-            var highestPoint = 1
+            var highestPoint = 0
             var supportedBy = mutableListOf<Int>()
             for (j in i - 1 downTo 0) {
                 val droppedBrick = bricks[j]
                 if (!(droppedBrick[0] > brick[3] || droppedBrick[3] < brick[0] ||
                         droppedBrick[1] > brick[4] || droppedBrick[4] < brick[1])
                 ) {
-                    val supportedHeight = droppedBrick[5] + 1
+                    val supportedHeight = droppedBrick[5]
                     if (supportedHeight > highestPoint) {
                         supportedBy = arrayListOf(j)
                         highestPoint = supportedHeight
@@ -38,8 +29,7 @@ object Day22 {
             if (supportedBy.size == 1) {
                 brickSet.remove(supportedBy.first())
             }
-            supportedList[i] = supportedBy
-            val dif = brick[2] - highestPoint
+            val dif = brick[2] - highestPoint - 1
             brick[2] = brick[2] - dif
             brick[5] = brick[5] - dif
         }
@@ -48,15 +38,7 @@ object Day22 {
     }
 
     fun sumOfDropsWhenRemoved(input: String): Int {
-        val bricks = input.trim().split('\n').map { line ->
-            val (start, end) = line.split('~').map { it.split(',').map { it.toInt() } }
-            val points = IntArray(6)
-            for (i in 0 until 3) {
-                points[i] = start[i].coerceAtMost(end[i])
-                points[i + 3] = start[i].coerceAtLeast(end[i])
-            }
-            points
-        }.sortedBy { it[2] }
+        val bricks = parseBricks(input)
 
         val supportedByList = Array<List<Int>>(bricks.size) { emptyList() }
         val supports = Array<ArrayList<Int>>(bricks.size) { ArrayList() }
@@ -104,4 +86,15 @@ object Day22 {
             droppedBricks.size - 1
         }
     }
+
+    // List of (x1,y1,z1,x2,y2,z2) where x1/y1/z1 will always be lower or equal then x2/y2/z2
+    private fun parseBricks(input: String): List<IntArray> = input.trim().split('\n').map { line ->
+        val (start, end) = line.split('~').map { it.split(',').map(String::toInt) }
+        val points = IntArray(6)
+        for (i in 0 until 3) {
+            points[i] = start[i].coerceAtMost(end[i])
+            points[i + 3] = start[i].coerceAtLeast(end[i])
+        }
+        points
+    }.sortedBy { it[2] }
 }
